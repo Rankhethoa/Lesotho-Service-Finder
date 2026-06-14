f649214359c59403ed6ed837f6e0c1bbee16e07b
Rankhethoa <rankhethoa.mp@gmail.com>
2026-06-14 19:51:02 +0800

    Fix static file serving

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Serve static frontend files
+app.use(express.static(path.join(__dirname)));
 app.use(express.static(path.join(__dirname, 'frontend')));
 
 
